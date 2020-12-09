9b97fdae457042e90810ce3b071ef6575485f954
AntigoneRandy <antigonerandy@qq.com>
2020-12-09 20:56:32 +0800

    优化决策树数据

--- javascript/DecisionTree.js
+++ javascript/DecisionTree.js
@@ -42,29 +42,37 @@
 
 // dic = {name:"未知",nation: "未知",game: "未知",championship: "未知",team:"未知",famousFor:"未知",award:"未知",character:"未知",working:};
 var models = [
-    ["N0tail", "丹麦", "Dota2", "外赛区", "OG", "他的亚巴顿", "18，19年Dota2国际邀请赛冠军", "辅助", true],
-    ["Zunba", "韩国", "守望先锋", "外赛区", "首尔王朝队", "他的查丽娅", "16,17年世界杯冠军","坦克",false],
-    ["JJonak", "韩国", "守望先锋", "外赛区", "纽约九霄天擎队", "他的禅雅塔", "世界最强禅雅塔","辅助", true],
-    ["miro", "韩国", "守望先锋", "外赛区", "首尔王朝队", "他的温斯顿", "2016年第一届守望先锋世界杯冠军", "坦克", true],
-    ["sky", "中国", "魔兽争霸", "中国", "WE", "当过奥运火炬手", "中国电竞第一人", "无", false],
-    ["moon", "韩国", "魔兽争霸", "外赛区", "WeMade FOX", "年龄超过30岁还仍然夺冠", "2018年黄金联赛冬季总决赛《魔兽争霸3》冠军", "无", true],
-    ["kuroky", "德国", "DOTA2", "外赛区", "Team Nigma", "他的拉比克", "2017年Dota2国际邀请赛冠军", "辅助", true],
-    ["faker", "韩国", "英雄联盟", "LCK", "SKT", "被称为大魔王", "三冠王", "中单", true],
-    ["the shy", "韩国", "英雄联盟", "LPL", "IG", "天神下凡一锤四","2018年世界总决赛冠军",  "上单", true],
-    ["Rookie", "韩国", "英雄联盟", "LPL", "IG", "他的球女","2018年世界总决赛冠军", "中单",  true],
-    ["Doinb", "韩国", "英雄联盟", "LPL", "FPX", "他的潘森和克烈", "2019年世界总决赛冠军", "中单", true],
-    ["doublelift", "美国", "英雄联盟", "LCS", "TSM", "他年年十六强", "LCS霸主", "下路", false],
-    ["karsa", "中国", "英雄联盟", "LPL", "TES", "外号“雷达哥”", "2018年MSI冠军", "打野", true],
-    ["tian", "中国", "英雄联盟", "LPL", "FPX", "他的琪亚娜", "2019年世界总决赛冠军", "打野", true],
-    ["ning", "中国", "英雄联盟", "LPL", "IG", "拿过总决赛mvp", "2018年世界总决赛冠军", "打野", true],
-    ["xiaohu", "中国", "英雄联盟", "LPL", "RNG", "打过2200的伤害", "2018年MSI冠军", "中单", true],
-    ["ming", "中国", "英雄联盟", "LPL", "RNG", "他是队伍的团宠", "2018年MSI冠军", "下路", true],
-    ["Weixiao", "中国", "英雄联盟", "LPL", "WE", "多年后重回原先的俱乐部执教", "IPL5冠军", "下路", false],
-    ["Uzi", "中国", "英雄联盟", "LPL", "RNG", "他的薇恩", "2018年MSI冠军", "下路", false],
-    ["Clearlove", "中国", "英雄联盟", "LPL", "WE", "他的伊芙琳","2015年MSI冠军",  "打野", false],
-    ["mlxg", "中国", "英雄联盟", "LPL", "RNG", "他的梦魇和奥拉夫", "2018年MSI冠军", "打野", false],
-    ["letme", "中国", "英雄联盟", "LPL", "RNG", "他的奥恩", "2018年MSI冠军", "未知", false]
+    ["N0tail", "丹麦", "Dota2", "外赛区", "OG", false, "国际邀请赛冠军", "辅助", true],
+    ["Zunba", "韩国", "守望先锋", "外赛区", "首尔王朝队", false, "世界杯冠军", "坦克", false],
+    ["JJonak", "韩国", "守望先锋", "外赛区", "纽约九霄天擎队", true, "世界最强禅雅塔", "辅助", true],
+    ["miro", "韩国", "守望先锋", "外赛区", "首尔王朝队", true, "世界杯冠军", "坦克", true],
+    ["sky", "中国", "魔兽争霸", "中国", "无", false, "中国电竞第一人", "无", false],
+    ["moon", "韩国", "魔兽争霸", "外赛区", "WeMade FOX", false, "黄金联赛冠军", "无", true],
+    ["kuroky", "德国", "DOTA2", "外赛区", "Team Nigma", false, "国际邀请赛冠军", "辅助", true],
+    ["faker", "韩国", "英雄联盟", "LCK", "SKT", true, "三冠王", "中单", true],
+    ["the shy", "韩国", "英雄联盟", "LPL", "IG", false, "世界总决赛冠军", "上单", true],
+    ["Rookie", "韩国", "英雄联盟", "LPL", "IG", false, "世界总决赛冠军", "中单", true],
+    ["Doinb", "韩国", "英雄联盟", "LPL", "FPX", false, "世界总决赛冠军", "中单", true],
+    ["doublelift", "美国", "英雄联盟", "LCS", "TSM", false, "LCS冠军", "下路", false],
+    ["karsa", "中国", "英雄联盟", "LPL", "TES", false, "MSI冠军", "打野", true],
+    ["tian", "中国", "英雄联盟", "LPL", "FPX", false, "世界总决赛冠军", "打野", true],
+    ["Showmaker", "韩国", "英雄联盟", "LCK", "DWG", true, "世界总决赛冠军", "中单", true],
+    ["Nuguri", "韩国", "英雄联盟", "LCK", "DWG", true, "世界总决赛冠军", "上单", true],
+    ["bin", "中国", "英雄联盟", "LPL", "SN", true, "世界总决赛亚军", "上单", true],
+    ["gimgoon", "韩国", "英雄联盟", "LPL", "FPX", true, "世界总决赛冠军", "上单", true],
+    ["scout", "韩国", "英雄联盟", "LPL", "EDG", true, "LPL冠军", "中单", true],
+    ["meiko", "中国", "英雄联盟", "LPL", "EDG", true, "LPL冠军", "辅助", true],
+    ["ning", "中国", "英雄联盟", "LPL", "IG", true, "世界总决赛冠军", "打野", true],
+    ["xiaohu", "中国", "英雄联盟", "LPL", "RNG", false, "MSI冠军", "中单", true],
+    ["ming", "中国", "英雄联盟", "LPL", "RNG", false, "MSI冠军", "下路", true],
+    ["Weixiao", "中国", "英雄联盟", "LPL", "WE", false, "IPL5冠军", "下路", false],
+    ["Uzi", "中国", "英雄联盟", "LPL", "RNG", true, "世界总决赛亚军", "下路", false],
+    ["Clearlove", "中国", "英雄联盟", "LPL", "EDG", false, "MSI冠军", "打野", false],
+    ["Jackeylove", "中国", "英雄联盟", "LPL", "TES", false, "世界总决赛冠军", "下路", true],
+    ["mlxg", "中国", "英雄联盟", "LPL", "RNG", true, "MSI冠军", "打野", false],
+    ["letme", "中国", "英雄联盟", "LPL", "RNG", false, "MSI冠军", "未知", false]
 ];
+
 function work() {
     if (models.length == 1) {
         alert("我猜他是" + models[0][0]);
@@ -74,12 +82,20 @@ function work() {
         alert("你猜的人不在名人堂里");
         return;
     }
-    var count = [[{ name: "", count: 0 }]]//[new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary() };
+    var count = [
+        [{
+            name: "",
+            count: 0
+        }]
+    ] //[new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary(),new Dictionary() };
     var j = 0;
     for (j = 0; j < 9; j++) {
         for (i = 0; i < models.length; i++) {
             if (!count[j]) {
-                count.push([{ name: models[i][j], count: 1 }]);
+                count.push([{
+                    name: models[i][j],
+                    count: 1
+                }]);
             } else {
                 var k = found(count[j], models[i][j]);
                 if (k != -1) count[j][k].count++;
@@ -100,7 +116,10 @@ function work() {
     //     })
     //     console.log("");
     // })
-    var max = { name: "", count: "" }
+    var max = {
+        name: "",
+        count: ""
+    }
     var countBest = [max];
     for (i = 1; i < count.length; i++) {
         countBest[i] = getBest(count[i]);
@@ -123,19 +142,19 @@ function work() {
             res = confirm("你所想的人参加" + question.name + "联赛吗？")
             break;
         case 4:
-            res = confirm("你所想的人属于或曾属于" + question.name + "战队吗？")
+            res = confirm("你所想的人退役前或现在在" + question.name + "战队吗？")
             break;
         case 5:
-            res = confirm("你所想的人因为" + question.name + "而出名吗？")
+            res = confirm("你所想的人" + (question.name ? "" : "不") + "戴眼镜吗？(以名人堂上的照片为准)")
             break;
         case 6:
-            res = confirm("你所想的人曾获" + question.name + "吗？")
+            res = confirm("他得过的最高奖项是不是" + question.name + "？")
             break;
         case 7:
             res = confirm("你所想的人是" + question.name + "选手吗？")
             break;
         case 8:
-            res = confirm("你所想的人" + question.name ? "在役" : "已经退役了" + "吗？")
+            res = confirm("你所想的人" + (question.name ? "在役" : "已经退役了") + "吗？")
             break;
         default:
             break;
@@ -169,7 +188,11 @@ function found(arr, name) {
 }
 
 function getMax(arr) {
-    var maxObj = { name: "", count: 0, index: 0 };
+    var maxObj = {
+        name: "",
+        count: 0,
+        index: 0
+    };
     for (var j = 0; j < arr.length; j++) {
         if (arr[j].count > maxObj.count) {
             maxObj = arr[j];
@@ -180,7 +203,11 @@ function getMax(arr) {
 }
 
 function getBest(arr) {
-    var bestObj = { name: "", count: 0, index: 0 };
+    var bestObj = {
+        name: "",
+        count: 0,
+        index: 0
+    };
     var distance = [];
     var minDistance = Math.floor(models.length / 2);
     for (var j = 0; j < arr.length; j++) {
@@ -193,5 +220,6 @@ function getBest(arr) {
             minDistance = distance[j];
         }
     }
+    console.log("best:" + JSON.stringify(bestObj));
     return bestObj;
 }
